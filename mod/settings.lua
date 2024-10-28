02a04b570300cea57b9e9490e6620ac12f12a506
celestialorb <chris.m.busby@gmail.com>
2024-10-28 03:58:24 +0000

    removing Factorio mod

--- mod/settings.lua
+++
@@ -1,9 +0,0 @@
-data:extend({
-    {
-        type = "int-setting",
-        name = "prometheus-exporter-metrics-update-every-nth-tick",
-        setting_type = "startup",
-        minimum_value = 1,
-        default_value = 60
-    }
-})
